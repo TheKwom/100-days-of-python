9c3d84a3f475e5fd14637cdb35750ded2e7f05ff
TheKwom <dafwendz@gmail.com>
2024-09-08 19:53:06 -0600

    first day complete

---
+++ day1.py
@@ -0,0 +1,6 @@
+# Day 1 Project - Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+cityName = input("What is the name of the city you grew up in?\n")
+petName = input("What is the name of your pet?\n")
+print("You could be " + cityName + " " + petName)
