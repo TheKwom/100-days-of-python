7fc23627d1d9edfc654e3d299cba985018089560
TheKwom <dafwendz@gmail.com>
2024-09-12 14:22:53 -0600

    Finished day4 project

--- day4.py
+++ day4.py
@@ -1,16 +1,27 @@
 # Day 4 - Rock, Paper, Scissors
 import random
 
-
-print(random.randint(0, 2))
-
-
-userChoice = int(input("What do you choose? Type 0 for rock, 1 for paper or 2 for scissors."))
+userChoice = int(input("What do you choose? Type 0 for rock, 1 for paper or 2 for scissors.\n"))
 compChoice = random.randint(0,2)
 
-print("You chose " + userChoice + ". Computer chose " + compChoice + ".")
+resultsMessage = ""
+game_strings = ["rock", "paper", "scissors"]
 
-# if userChoice == 0 
+if 0 <= userChoice <= 2:
+    print("You chose " + game_strings[userChoice] + ".")
+    print("Computer chose " + game_strings[compChoice] + ".")
 
+if userChoice < 0 or userChoice > 2:
+    print("Invalid input. You lose")
+elif userChoice == 0 and compChoice == 2:
+    resultsMessage = "You win!"
+elif compChoice == 0 and userChoice == 2:
+    resultsMessage = "You lose."
+elif userChoice < compChoice:
+    resultsMessage = "You lose."
+elif userChoice > compChoice:
+    resultsMessage = "You win!"
+elif userChoice == compChoice:
+    resultsMessage = "Draw."
 
-print
+print(resultsMessage)
