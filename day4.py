7eb073e18d90cb59699d8f5577733005a8429ba7
TheKwom <dafwendz@gmail.com>
2024-09-11 23:51:11 -0600

    beginning of day4 project

---
+++ day4.py
@@ -0,0 +1,16 @@
+# Day 4 - Rock, Paper, Scissors
+import random
+
+
+print(random.randint(0, 2))
+
+
+userChoice = int(input("What do you choose? Type 0 for rock, 1 for paper or 2 for scissors."))
+compChoice = random.randint(0,2)
+
+print("You chose " + userChoice + ". Computer chose " + compChoice + ".")
+
+# if userChoice == 0 
+
+
+print
